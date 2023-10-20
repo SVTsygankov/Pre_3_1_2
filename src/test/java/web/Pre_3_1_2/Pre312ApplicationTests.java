388496de960872607edcf99f3a2e157377da4c03
SVTsygankov <s.v.tsygankov@mail.ru>
2023-10-20 11:21:22 +0500

    Пустое Boot приложение

---
+++ src/test/java/web/Pre_3_1_2/Pre312ApplicationTests.java
@@ -0,0 +1,13 @@
+package web.Pre_3_1_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pre312ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
